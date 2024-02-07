9ec48bdc77bcabc6737aea98c55c48af9bc126ce
Tobias Grosse-Puppendahl <tobias@grosse-puppendahl.de>
2024-02-07 20:25:32 +0100

    initial commit

---
+++ Formulas/accessory-switcher-service.rb
@@ -0,0 +1,31 @@
+class AccessorySwitcherService < Formula
+  desc "Accessory Switcher Service"
+  homepage "https://github.com/tobias-gp/accessory-switcher"
+  
+  url "https://github.com/tobias-gp/accessory-switcher/releases/download/v0.0.19/bundle.tar.gz"
+  sha256 "1e739a0b0eedf105ed72903cf9d0194a9bd16a02656fc39b7eb173f28a22bf22"
+  version "0.0.19"
+
+  depends_on "python@3.9"
+  depends_on "blueutil"
+
+  def install
+    # Install the service
+    (lib/"AccessorySwitcher").install Dir["*"]
+
+    # Create a script to start the service
+    (bin/"AccessorySwitcher").write <<~EOS
+      #!/bin/sh
+      exec "#{HOMEBREW_PREFIX}/bin/python3" "#{opt_prefix}/lib/AccessorySwitcher/main.py" "$@"
+    EOS
+  end
+
+  service do
+    run opt_bin/"AccessorySwitcher"
+    keep_alive true
+  end
+
+  test do
+    system "#{bin}/AccessorySwitcher", "--help"
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# homebrew-accessory-switcher
-Homebrew Accessory Switcher
+# Homebrew Accessory Switcher
+
+This project caters to users with multiple Mac devices who wish to interchangeably use their Bluetooth keyboard and mouse across these devices.
+
+Please note that this repository is specifically for the Homebrew package. The source code for the project can be accessed at: https://github.com/tobias-gp/homebrew-accessory-switcher
